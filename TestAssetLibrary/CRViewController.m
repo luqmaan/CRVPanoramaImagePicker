527f78cc079809eceda92d30a9fa1d55d0bce474
Luqmaan <ldawoodjee@gmail.com>
2013-01-05 17:30:34 -0500

    Added buttons, not functional yet

--- TestAssetLibrary/CRViewController.m
+++ TestAssetLibrary/CRViewController.m
@@ -27,6 +27,8 @@
     scrollViewContentSize = [[UIScreen mainScreen] bounds].size;
         
     __block NSInteger top = 0;
+    __block CGFloat viewWidth = scrollViewContentSize.width;
+    __block CGRect buttonFrame = CGRectMake(viewWidth - 45, 15,35,35);
     
     [panoramaImageFinder findPanoramaImagesAndPerformCallback:^(ALAsset *panoramaImageRef) {
         
@@ -35,43 +37,46 @@
             Slower: [panoramaImageRef defaultRepresentation] fullScreenImage]
             Slowest: [panoramaImageRef defaultRepresentation] fullResolutionImage]
          */
+        // create the thumbnail and image view
         UIImage *thumb = [UIImage imageWithCGImage:[[panoramaImageRef defaultRepresentation] fullScreenImage]];
-
         UIImageView *imageView = [[UIImageView alloc] initWithImage:thumb];
         
-        CGFloat viewWidth = [[UIScreen mainScreen] bounds].size.width;
+        // set the dimensions of the thumbnail
         CGSize dimensions = [[panoramaImageRef defaultRepresentation] dimensions];
         CGFloat aspectRatio = dimensions.height / dimensions.width ;
         CGFloat height = aspectRatio * viewWidth;
+        CGRect imageFrame = CGRectMake(0, top, viewWidth, height);        
+        NSLog(@"%@", NSStringFromCGRect(imageFrame));
         
-        CGRect frame = CGRectMake(0, top, viewWidth, height);
-        
-        NSLog(@"%@", NSStringFromCGRect(frame));
-        
-        [imageView setFrame:frame];
+        // show the thumbnail
+        [imageView setFrame:imageFrame];
         [[self view] addSubview:imageView];
-        
 
+        // create the action button
+        UIButton *button  = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+        buttonFrame.origin = CGPointMake(viewWidth - 45, (top + (height / 2)) - 17.5);
+        [button setFrame:buttonFrame];
+        [button addTarget:self action:@selector(chooseImage) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:button];
+        
+//        [imageView addGestureRecognizer:<#(UIGestureRecognizer *)#>]
+
+        // update the size of the scrollview and the top position of the next image
         top += height;
         scrollViewContentSize.height = top;
         [[self scrollView] setContentSize:scrollViewContentSize];
         
-        
     }];
 
-    
 }
-
-- (void)presentPanoramasWithGroup:(ALAssetsGroup *) groupOfPanoramas
-{
-    NSLog(@"hai");
-}
-
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+- (void)chooseImage
+{
+    NSLog(@"clicked on button");
+}
 
 @end
