b21c7d819ef47d034fe5b5de33f5b2135320066b
Luqmaan <ldawoodjee@gmail.com>
2013-01-07 13:07:00 -0500

    Setting up new structure

---
+++ CRVPanoramaImagePicker/CRVPanoramaImagePicker.h
@@ -0,0 +1,13 @@
+//
+//  CRVPanoramaImagePicker.h
+//  PanoramaImagePicker
+//
+//  Created by Lolcat on 07/01/2013.
+//  Copyright (c) 2013 Createch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CRVPanoramaImagePicker : UIViewController
+
+@end
---
+++ CRVPanoramaImagePicker/CRVPanoramaImagePicker.m
@@ -0,0 +1,38 @@
+//
+//  CRVPanoramaImagePicker.m
+//  PanoramaImagePicker
+//
+//  Created by Lolcat on 07/01/2013.
+//  Copyright (c) 2013 Createch. All rights reserved.
+//
+
+#import "CRVPanoramaImagePicker.h"
+
+@interface CRVPanoramaImagePicker ()
+
+@end
+
+@implementation CRVPanoramaImagePicker
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
